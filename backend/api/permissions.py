b05cf2dc297311ef31c5bbd33266d36c740a8e32
Olga Yagnyuk <olga07122007@yandex.ru>
2023-06-02 16:53:21 +0300

    add

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,6 +2,7 @@ from rest_framework import permissions
 
 
 class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
+    '''Имеет право доступа только автор или админ.'''
     def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -3,11 +3,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf.urls import url
+from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path("api/", include("recipes.urls")),
+    #url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 
@@ -15,3 +20,4 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
+
