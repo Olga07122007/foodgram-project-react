d5f181c891cc0d7e57c621539d5dccafa22074a6
Olga Yagnyuk <olga07122007@yandex.ru>
2023-06-02 17:10:51 +0300

    settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
 SECRET_KEY = os.getenv('SECRET_KEY', default='Django secret key')
 
 
