0e2cf7ad4c29abb93a72eaeefde1fe196367b8da
Olga Yagnyuk <olga07122007@yandex.ru>
2023-06-07 17:28:42 +0300

    fix_settings_DEBUG

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='Django secret key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', default=True)
+DEBUG = os.getenv('DEBUG', default=False)
 
 
 ALLOWED_HOSTS = ['*']
